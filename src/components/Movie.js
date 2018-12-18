1436332376569f718c147e32daceef708af74905
Kat Perreira <kat.perreira@gmail.com>
2018-12-18 14:06:37 -0800

    got app.js, movie library, and movie to render

---
+++ src/components/Movie.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import './Movie.css';
+
+class Movie extends Component {
+  constructor(props) {
+    super(props);
+
+    // movie params: id, title, overview, release_date, image_url, external_id
+    this.state = {
+      id: this.props.id,
+      title: this.props.title,
+      overview: this.props.overview,
+      release_date: this.props.release_date,
+      image: this.props.image_url,
+      external_id: this.props.external_id,
+
+    }
+  }
+
+  render(){
+    return(
+      <ul>
+        <li>this.props.title</li>
+      </ul>
+    )
+  }
+}
+
+Movie.propTypes = {
+  id: PropTypes.string,
+  title: PropTypes.string,
+  overview: PropTypes.string,
+  release_date: PropTypes.string,
+  image: PropTypes.string,
+  external_id: PropTypes.string,
+};
+
+export default Movie;
---
+++ src/components/MovieLibrary.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import axios from 'axios';
+
+import './MovieLibrary.css';
+import Movie from './Movie';
+
+
+class MovieLibrary extends Component {
+  constructor() {
+    super();
+
+    // initial state is an empty array
+    this.state = {
+      movies: [],
+    };
+  }
+
+  componentDidMount() {
+    console.log("hi maddy I am mounting the DOM");
+    const GET_THE_LIBRARY_MOVIES = "http://localhost:3000/movies";
+
+    // promise
+    axios.get(GET_THE_LIBRARY_MOVIES)
+    .then((response)=> {
+      this.setState({
+        movies: response.data,
+      });
+    })
+    // if nothing is retrieved, cancel Xmas
+    .catch((error) => {
+      this.setState({
+        error: error.message
+      });
+    });
+  }
+
+  // movie params: id, title, overview, release_date, image_url, external_id
+  render() {
+    let movieList = this.state.movies;
+
+    // is this nested? how do I call movie? ask maddy when she wakes up
+    const list = movieList.map((movie) => {
+      return <Movie
+      key={movie.id}
+      id={movie.id}
+      title={movie.title}
+      overview={movie.overview}
+      release_date={movie.release_date}
+      image={movie.image_url}
+      />
+    });
+
+    return (
+      <div className="movie-list">
+      { list }
+      </div>
+    )
+  }
+}
+
+MovieLibrary.propTypes = {
+
+};
+
+export default MovieLibrary;
