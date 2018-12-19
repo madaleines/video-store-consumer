1b97f1065f68afb0859dc3441c594116683fa541
Kat Perreira <kat.perreira@gmail.com>
2018-12-19 11:46:48 -0800

    search form stuff

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -6,7 +6,6 @@ import './SearchForm.css';
 class SearchForm extends Component {
   constructor(props) {
     super(props);
-
     const resetState = {
       query: '',
       searchResults: []
@@ -25,15 +24,21 @@ class SearchForm extends Component {
 
   submitForm = (event) => {
     event.preventDefault();
-    const query = {...this.state};
+    const query = this.state.query;
     const SEARCH_URL = `http://localhost:3000/movies?query=${query}`;
+    console.log(SEARCH_URL)
     axios.get(SEARCH_URL)
     .then((response) => {
-      this.setState({searchResults: response.data})
+      console.log(response)
+
+      this.setState({
+        searchResults: response.data
+      })
     })
     .catch((error) => {
       this.setState({ message: error.message});
     })
+
 
     console.log(query)
     console.log(this.state.searchResults)
@@ -45,13 +50,14 @@ class SearchForm extends Component {
   render() {
     return (
       <section className="search-form">
-        <h3 className="search-form__header">Create a New Inspiration</h3>
+        <h3 className="search-form__header"></h3>
         <form onSubmit={ this.submitForm } className="search-form__form">
           <label htmlFor="text" className="search-form__form-label">Movie Title: </label>
           <textarea name="query" value={this.state.query}
-          onChange={ this.onFieldChange } className="search-form__form-textarea"/>
+            onChange={ this.onFieldChange } className="search-form__form-text"/>
           <input type="submit" value="Search" className="search-form__form-button"/>
         </form>
+        <div>{this.state.searchResults.length}</div>
       </section>
     )
   }
